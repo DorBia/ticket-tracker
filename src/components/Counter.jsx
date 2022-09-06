0d42a39aa2a9fb655aa97e567f9e5ae231b1ae91
DorBia <dorota.bialy95@gmail.com>
2022-09-06 19:22:15 +0100

    Added working counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./Counter.scss"
 
 const Counter = ({id}) => {
+    const [count, setCount] = useState(0);
+    const handleAdd = () => setCount((current) => current + 1)
+
+    const handleRemove = () => count === 0 ? void(0) : setCount((current) => current - 1)
 
   return (
     <div className="counter" key={id}>
         <h4>Counter</h4>
-        <p>0</p>
-        <button>-</button>
-        <button>+</button>
+        <p>{count}</p>
+        <button onClick={() => handleRemove()}>-</button>
+        <button onClick={() => handleAdd()}>+</button>
     </div>
   )
 }
