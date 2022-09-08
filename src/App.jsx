f84564c501249e1645ab1d83b9027389d2b869ea
DorBia <dorota.bialy95@gmail.com>
2022-09-08 16:17:57 +0100

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,45 @@ import './App.scss';
 import AddNewEmployee from './components/AddNewEmployee';
 import Card from './components/Card';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 import team from "./data/team";
 
 function App() {
   const [isActive, setIsActive] = useState(false)
   const [employees, setEmployees] = useState(team)
   const [searchText, setSearchText] = useState("")
+  const [page, setPage] = useState(0);
 
-  const addEmployee = (employee) => {
-    setEmployees((employees) => [...employees, employee])
-  }
+  // pages logic
+  const employeesPerPage = 10;
+  const seenEmployees = page * employeesPerPage;
 
-  const handleInput = (e) => {
-    setSearchText(e.target.value.toLowerCase()); 
-  }
+  const displayEmployees = employees.slice(seenEmployees, seenEmployees + employeesPerPage)
+
+  const pageCount = Math.ceil(employees.length / employeesPerPage);
+
+  const handlePageChange = (e) => setPage(e.selected);
+
+  // add employee logic
+  const addEmployee = (employee) => setEmployees((employees) => [...employees, employee])
+
+  // search logic
+  const handleInput = (e) => setSearchText(e.target.value.toLowerCase())
 
   const filteredEmployees = employees.filter((employee) => {
     return employee.name.toLowerCase().includes(searchText) || employee.role.toLowerCase().includes(searchText);
   });
 
-  const handleDelete = (id) => {
-    setEmployees(employees.filter((e) => e.id !== id))
-  }
+  const searchPageCount = Math.ceil(filteredEmployees.length / employeesPerPage);
+
+  // delete logic
+  const handleDelete = (id) => setEmployees(employees.filter((e) => e.id !== id))
 
   return (
     <div className="app">
-      <Header setIsActive={setIsActive}/>
+      <Header setIsActive={setIsActive} handleInput={handleInput}/>
       {isActive && <AddNewEmployee addEmployee={addEmployee} setIsActive={setIsActive}/>}
-      {!searchText && <Card employees={employees} handleDelete={handleDelete}/>}
-      {searchText && <Card employees={filteredEmployees} handleDelete={handleDelete}/>}
-      <SearchBar handleInput={handleInput}/>
+      {!searchText && <Card employees={displayEmployees} handleDelete={handleDelete} handlePageChange={handlePageChange} pageCount={pageCount}/>}
+      {searchText && <Card employees={filteredEmployees} handleDelete={handleDelete} handlePageChange={handlePageChange} pageCount={searchPageCount}/>}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,34 @@
 import Counter from "./Counter"
+import ReactPaginate from "react-paginate";
 
 import "./Card.scss"
 
-const Card = ({employees, handleDelete}) => {
+const Card = ({employees, handleDelete, handlePageChange, pageCount}) => {
 
   return (
-    <div className="card-container"> 
+    <>
+      <div className="card-container"> 
         {employees.map((event) => (
-            <div key={event.id} className="card">
-                <p className="delete" onClick={() => handleDelete(event.id)}>x</p>
-                <h2>{event.name}</h2>
-                <h3 className="role">{event.role}</h3>
-                <Counter id={event.id}/>
-            </div>
+          <div key={event.id} className="card">
+            <p className="delete" onClick={() => handleDelete(event.id)}>x</p>
+            <h2>{event.name}</h2>
+            <h3 className="role">{event.role}</h3>
+            <Counter id={event.id}/>
+          </div>
         ))}
-    </div>
-
+      </div>
+      
+      <ReactPaginate
+        className={"pagination"}
+        pageCount={pageCount}
+        previousLabel={"Previous"}
+        previousLinkClassName={"previous"}
+        nextLabel={"Next"}
+        nextLinkClassName={"next"}
+        onPageChange={handlePageChange}
+        activeClassName={"active"}
+      />
+    </>
   )
 }
 
